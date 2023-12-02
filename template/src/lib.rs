5501892a97c4a5f01d6ccac5cd6e69c7a7ff7c31
Tanguy Soubry <tanguy.soubry@edpnet.be>
2023-12-02 07:21:21 +0100

    fix template

--- template/src/lib.rs
+++ template/src/lib.rs
@@ -2,10 +2,10 @@ pub mod data;
 
 pub use crate::data::*;
 
-// fn answer_part1(data: Vec<Parsed>) -> AnswerDtype {
+// pub fn answer_part1(data: Vec<Parsed>) -> AnswerDtype {
 //     todo!()
 // }
 
-// fn answer_part2(data: Vec<Parsed>) -> AnswerDtype {
+// pub fn answer_part2(data: Vec<Parsed>) -> AnswerDtype {
 //     todo!()
 // }
