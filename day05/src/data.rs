0571799532a6bca6df5cb95c07995c2eb5c00152
Tanguy Soubry <tanguy.soubry@edpnet.be>
2023-12-05 08:20:35 +0100

    really slow solution part 1

--- day05/src/data.rs
+++ day05/src/data.rs
@@ -1,13 +1,14 @@
 pub type AnswerDtype = u64;
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub struct Range {
-    destination_range_start: AnswerDtype,
-    source_range_start: AnswerDtype,
-    range_length: AnswerDtype,
+use std::ops::Range;
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct MapData {
+    pub destination_range_start: AnswerDtype,
+    pub source_range: Range<AnswerDtype>,
 }
 
-pub type Parsed = (Vec<AnswerDtype>, Vec<Vec<Range>>);
+pub type Parsed = (Vec<AnswerDtype>, Vec<Vec<MapData>>);
 
 fn split_into_numbers(input: &str) -> Vec<AnswerDtype> {
     input
@@ -27,20 +28,24 @@ pub fn import_data(data: &str) -> Parsed {
         .map(|s| {
             let mut lines = s.lines();
             let _ = lines.next().expect("first line"); // skip first line
-            lines.map(|line| parse(line)).collect::<Vec<Range>>()
+            lines.map(|line| parse(line)).collect::<Vec<MapData>>()
         })
         .collect();
 
     (seeds, maps)
 }
 
-pub fn parse(line: &str) -> Range {
+pub fn parse(line: &str) -> MapData {
     let split_white = split_into_numbers(line);
 
-    Range {
-        destination_range_start: *split_white.get(0).expect("get range item"),
-        source_range_start: *split_white.get(1).expect("get range item"),
-        range_length: *split_white.get(2).expect("get range item"),
+    let destination_range_start = *split_white.get(0).expect("get range item");
+    let source_range_start = *split_white.get(1).expect("get range item");
+    let range_length = *split_white.get(2).expect("get range item");
+
+
+    MapData {
+        destination_range_start: destination_range_start,
+        source_range: source_range_start..(source_range_start + range_length),
     }
 }
 
@@ -77,7 +82,7 @@ temperature-to-humidity map:
 humidity-to-location map:
 60 56 37
 56 93 4"#;
-pub const TEST_ANSWER_1: AnswerDtype = 0;
+pub const TEST_ANSWER_1: AnswerDtype = 35;
 
 pub const TEST_DATA_2: &str = TEST_DATA_1;
 pub const TEST_ANSWER_2: AnswerDtype = 0;
--- day05/src/lib.rs
+++ day05/src/lib.rs
@@ -2,10 +2,38 @@ pub mod data;
 
 pub use crate::data::*;
 
-// pub fn answer_part1(data: Vec<Parsed>) -> AnswerDtype {
-//     todo!()
-// }
+fn get_mapped_number(maps: &mut Vec<MapData>, item: AnswerDtype) -> AnswerDtype {
+    for map_data in maps {
+        if let Some(mapped_number) = map_data
+            .source_range
+            .position(|x| x == item)
+            .map(|idx| map_data.destination_range_start + idx as u64)
+        {
+            return mapped_number;
+        }
+    }
 
-// pub fn answer_part2(data: Vec<Parsed>) -> AnswerDtype {
+    item
+}
+
+pub fn answer_part1(data: Parsed) -> AnswerDtype {
+    let (seeds, maps) = data;
+
+    seeds
+        .into_iter()
+        .map(|seed| {
+            let mut item = seed;
+
+            for mut map in maps.clone() {
+                item = get_mapped_number(&mut map, item);
+            }
+
+            item
+        })
+        .min().expect("error getting minimum value")
+
+}
+
+// pub fn answer_part2(data: Parsed) -> AnswerDtype {
 //     todo!()
 // }
