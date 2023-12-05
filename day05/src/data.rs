db0bff5ef807f3320e3c3554135e0c1a17990ee8
Tanguy Soubry <tanguy.soubry@edpnet.be>
2023-12-05 07:28:18 +0100

    write parser day05

---
+++ day05/src/data.rs
@@ -0,0 +1,95 @@
+pub type AnswerDtype = u64;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct Range {
+    destination_range_start: AnswerDtype,
+    source_range_start: AnswerDtype,
+    range_length: AnswerDtype,
+}
+
+pub type Parsed = (Vec<AnswerDtype>, Vec<Vec<Range>>);
+
+fn split_into_numbers(input: &str) -> Vec<AnswerDtype> {
+    input
+        .split_whitespace()
+        .map(|s| s.parse::<AnswerDtype>().expect("error parse"))
+        .collect()
+}
+
+pub fn import_data(data: &str) -> Parsed {
+    let mut split_lines = data.split("\n\n").into_iter();
+
+    let seeds_str = split_lines.next().expect("seeds").trim_start_matches("seeds: ");
+
+    let seeds = split_into_numbers(seeds_str);
+
+    let maps: Vec<_> = split_lines
+        .map(|s| {
+            let mut lines = s.lines();
+            let _ = lines.next().expect("first line"); // skip first line
+            lines.map(|line| parse(line)).collect::<Vec<Range>>()
+        })
+        .collect();
+
+    (seeds, maps)
+}
+
+pub fn parse(line: &str) -> Range {
+    let split_white = split_into_numbers(line);
+
+    Range {
+        destination_range_start: *split_white.get(0).expect("get range item"),
+        source_range_start: *split_white.get(1).expect("get range item"),
+        range_length: *split_white.get(2).expect("get range item"),
+    }
+}
+
+pub const TEST_DATA_1: &str = r#"seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4"#;
+pub const TEST_ANSWER_1: AnswerDtype = 0;
+
+pub const TEST_DATA_2: &str = TEST_DATA_1;
+pub const TEST_ANSWER_2: AnswerDtype = 0;
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn test_parsing() {
+        let input_data = import_data(TEST_DATA_1);
+        println!("{:?}", input_data);
+    }
+}
---
+++ day05/src/lib.rs
@@ -0,0 +1,11 @@
+pub mod data;
+
+pub use crate::data::*;
+
+// pub fn answer_part1(data: Vec<Parsed>) -> AnswerDtype {
+//     todo!()
+// }
+
+// pub fn answer_part2(data: Vec<Parsed>) -> AnswerDtype {
+//     todo!()
+// }
