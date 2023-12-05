35fbbfec65f11e4ceaf526d8a45fda72891b69c4
Tanguy Soubry <tanguy.soubry@edpnet.be>
2023-12-05 10:34:07 +0100

    improve memory and performance

--- day05/src/lib.rs
+++ day05/src/lib.rs
@@ -2,6 +2,8 @@ pub mod data;
 
 pub use crate::data::*;
 
+use rayon::prelude::*;
+
 fn get_mapped_number_faster(maps: &Vec<MapData>, item: AnswerDtype) -> AnswerDtype {
     for map_data in maps {
         let start = map_data.source_range.start;
@@ -21,11 +23,11 @@ pub fn answer_part1(data: Parsed) -> AnswerDtype {
     let (seeds, maps) = data;
 
     seeds
-        .into_iter()
+        .into_par_iter()
         .map(|seed| {
             let mut item = seed;
 
-            for map in maps.clone() {
+            for map in &maps {
                 item = get_mapped_number_faster(&map, item);
             }
 
@@ -38,29 +40,19 @@ pub fn answer_part1(data: Parsed) -> AnswerDtype {
 pub fn answer_part2(data: Parsed) -> AnswerDtype {
     let (seed_info, maps) = data;
 
-    let seed_pairs = seed_info.chunks(2).map(|chunk| match chunk {
+    let seed_pairs: Vec<_> = seed_info.chunks(2).map(|chunk| match chunk {
         &[a, b] => (a, b),
         _ => panic!("error chunks"),
-    });
+    }).collect();
 
-    let seeds = seed_pairs
+    seed_pairs
+        .into_par_iter()
         .flat_map(|(start, len)| (start..(start + len)).collect::<Vec<_>>())
-        .collect::<Vec<_>>();
-
-    let total_seeds = seeds.len();
-
-    seeds
-        .into_iter()
-        .enumerate()
-        .map(|(handled, seed)| {
+        .map(|seed| {
             let mut item = seed;
 
-            for map in maps.clone() {
+            for map in &maps {
                 item = get_mapped_number_faster(&map, item);
-            }
-
-            if handled % 10000 == 0 {
-                println!("handled: {}%", handled as f64 / total_seeds as f64 * 100f64);
             }
 
             item
